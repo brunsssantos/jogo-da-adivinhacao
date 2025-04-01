00926e1b5f792caf5ef923972f23cb137b17eca1
Bruna Santos <brunasan7os@gmail.com>
2025-04-01 17:19:18 -0300

    Refatoração estruturada completa

--- Program.cs
+++ Program.cs
@@ -4,78 +4,91 @@
     {
         static void Main(string[] args)
         {
-            string escolhaDificuldade = ExibirMenu();
+            int totalTentativas = DefinirTentativas(ExibirMenu());
+            int numeroSecreto = GerarNumeroSecreto();
+            Jogar(totalTentativas, numeroSecreto);
 
-            int totalTentativas = 0;
+        }   
 
-            if (escolhaDificuldade == "1")
-                totalTentativas = 10;
-
-            else if (escolhaDificuldade == "2")
-                totalTentativas = 5;
-            else
-                totalTentativas = 3;
-
-
-            Random geradorDeNumeros = new Random();
-            int numeroSecreto = geradorDeNumeros.Next(1, 21);
-            int[] numerosDigitados = new int[totalTentativas];
-            int contadorHistorico = 0;
-
-            for (int tentativa = 1; tentativa <= totalTentativas; tentativa++)
+        static int DefinirTentativas(string escolhaDificuldade)
             {
-                Console.Clear();
-                Console.WriteLine("---------------------------------");
-                Console.WriteLine($"Tentativa {tentativa} de {totalTentativas}");
-                Console.WriteLine("---------------------------------");
+                int totalTentativas = 0;
 
-                if (contadorHistorico > 0)
-                {
-                    Console.WriteLine("Números digitados:");
-                    for (int contador = 0; contador < contadorHistorico; contador++)
-                    {
-                        Console.Write(numerosDigitados[contador] + " ");
-                    }
-                    Console.WriteLine();
-                    Console.WriteLine("---------------------------------");
+                if (escolhaDificuldade == "1")
+                    return 10;
 
-                }
-
-                Console.Write("Digite um número (de 1 a 20) para chutar: ");
-                int numeroDigitado = Convert.ToInt32(Console.ReadLine());
-                numerosDigitados[contadorHistorico] = numeroDigitado;
-                contadorHistorico++;
-
-                if (numeroDigitado == numeroSecreto)
-                {
-                    Console.WriteLine("---------------------------------");
-                    Console.WriteLine("Parabéns, você acertou!");
-                    Console.WriteLine("---------------------------------");
-                    break;
-                }
-
-                else if (numeroDigitado > numeroSecreto)
-                {
-                    Console.WriteLine("-------------------------------------------------");
-                    Console.WriteLine("O número digitado foi maior que o número secreto!");
-                    Console.WriteLine("-------------------------------------------------");
-                }
-
-                else
-                {
-                    Console.WriteLine("-------------------------------------------------");
-                    Console.WriteLine("O número digitado foi menor que o número secreto!");
-                    Console.WriteLine("-------------------------------------------------");
-                }
-
-                Console.WriteLine("Digite ENTER para continuar");
-                Console.ReadLine();
+                else if (escolhaDificuldade == "2")
+                    return 5;
+            else
+                return 3;
             }
 
-            Console.ReadLine();
-
-
+        static int GerarNumeroSecreto()
+        {
+            Random geradorDeNumeros = new Random();
+            int numeroSecreto = geradorDeNumeros.Next(1, 21);
+            return numeroSecreto;
         }
+
+        static void Jogar(int totalTentativas, int numeroSecreto)
+            {
+                int[] numerosDigitados = new int[totalTentativas];
+                int contadorHistorico = 0;
+
+                for (int tentativa = 1; tentativa <= totalTentativas; tentativa++)
+                {
+                    Console.Clear();
+                    Console.WriteLine("---------------------------------");
+                    Console.WriteLine($"Tentativa {tentativa} de {totalTentativas}");
+                    Console.WriteLine("---------------------------------");
+
+                    if (contadorHistorico > 0)
+                    {
+                        Console.WriteLine("Números digitados:");
+                        for (int contador = 0; contador < contadorHistorico; contador++)
+                        {
+                            Console.Write(numerosDigitados[contador] + " ");
+                        }
+                        Console.WriteLine();
+                        Console.WriteLine("---------------------------------");
+
+                    }
+
+                    Console.Write("Digite um número (de 1 a 20) para chutar: ");
+                    int numeroDigitado = Convert.ToInt32(Console.ReadLine());
+                    numerosDigitados[contadorHistorico] = numeroDigitado;
+                    contadorHistorico++;
+
+                    if (numeroDigitado == numeroSecreto)
+                    {
+                        Console.WriteLine("---------------------------------");
+                        Console.WriteLine("Parabéns, você acertou!");
+                        Console.WriteLine("---------------------------------");
+                        Console.ReadLine();
+                        break;
+                    }
+
+                    else if (numeroDigitado > numeroSecreto)
+                    {
+                        Console.WriteLine("-------------------------------------------------");
+                        Console.WriteLine("O número digitado foi maior que o número secreto!");
+                        Console.WriteLine("-------------------------------------------------");
+                    }
+
+                    else
+                    {
+                        Console.WriteLine("-------------------------------------------------");
+                        Console.WriteLine("O número digitado foi menor que o número secreto!");
+                        Console.WriteLine("-------------------------------------------------");
+                    }
+
+                    Console.WriteLine("Digite ENTER para continuar");
+                    Console.ReadLine();
+                }
+       
+            }
+
+
         static string ExibirMenu()
         {
             Console.WriteLine("----------------------------------");
